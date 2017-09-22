23dc46545ee4476249cc4a59956f0fcf4990e7d6
Aaron Vaughan <airon.vaughan@gmail.com>
2017-09-21 22:50:23 -0400

    First commit

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var buttonCount = 0
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        
+        print(buttonCount)
+        
+        if buttonCount >= 10 {
+            
+    
+        view.backgroundColor = UIColor.red
+        
+    
+        myLabel.text = "10 Times Unlocked"
+    
+        }
+        if buttonCount >= 15 {
+            
+            
+            view.backgroundColor = UIColor.blue
+            
+            myLabel.text = "15 Times Unlocked"
+            
+        }
+
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
